f222de87831095fa9619f68f08052529f262de4b
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-03-15 14:46:31 +0100

    bottom navbar responsive

--- src/components/Buttonsbottom.js
+++ src/components/Buttonsbottom.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withAuth } from '../components/AuthProvider';
-import { Navbar, Nav, Image } from 'react-bootstrap';
+import { Navbar, Nav, Image, NavItem } from 'react-bootstrap';
+import '../app.css';
 
 class Buttonsbottom extends Component {
   render() {
     const { isLogged } = this.props;
     if (isLogged) {
       return (
-        <Navbar bg="light" fixed="bottom" className="p-0">
-          <Nav className="mr-auto p0">
+
+        <Nav className="justify-content-center white fixed-bottom">
+          <NavItem>
             <Link to='/contacts'><Image src="/images/1.png" /></Link>
+          </NavItem>
 
-
+          <NavItem>
             <Link to='/scan' className="mr-1 ml-2"><Image src="/images/2.png" /></Link>
+          </NavItem>
 
-
+          <NavItem>
             <Link to='/tasks'><Image src="/images/3.png" /></Link>
-          </Nav>
-        </Navbar>
+          </NavItem>
+        </Nav>
 
       )
     } else {
@@ -30,3 +34,19 @@ class Buttonsbottom extends Component {
 }
 
 export default withAuth()(Buttonsbottom);
+
+{/* <Navbar bg="light" fixed="bottom" >
+<Nav className="justify-content-center green">
+  <NavItem>
+    <Link to='/contacts'><Image src="/images/1.png" /></Link>
+  </NavItem>
+
+  <NavItem>
+    <Link to='/scan' className="mr-1 ml-2"><Image src="/images/2.png" /></Link>
+  </NavItem>
+
+  <NavItem>
+    <Link to='/tasks'><Image src="/images/3.png" /></Link>
+  </NavItem>
+</Nav>
+</Navbar> */}
